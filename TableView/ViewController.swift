6c74b7fa4845167ee8c54cc46c976ceb523b9b11
Tal <talspektor.gmail.com>
2020-11-18 14:38:22 +0200

    Initial Commit

---
+++ TableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableView
+//
+//  Created by Tal Spektor on 18/11/2020.
+//  Copyright © 2020 Tal Spektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
